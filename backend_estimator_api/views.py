11d091d426ce6893a7753923017ba56b67d72f6e
Bernard Wambua <bernardmwambua12@gmail.com>
2020-04-14 22:21:00 +0300

    Some changes on urls and views

--- backend_estimator_api/views.py
+++ backend_estimator_api/views.py
@@ -1,29 +1,75 @@
+import datetime
 from rest_framework import status
+from rest_framework.mixins import ListModelMixin
+from rest_framework.viewsets import GenericViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import viewsets, generics
-from backend_estimator_api.estimator import estimator
 from backend_estimator_api.serializers import LogSerializer
 from backend_estimator_api.models import Log
+from dicttoxml import dicttoxml
+from backend_estimator_api.estimator import estimator
+
+
+def get_time_diff(start, end):
+    delta = end - start
+    return delta.total_seconds() * 1000
 
 
 class Estimator(APIView):
     """
-    Class view to return response from estimator module.
+        Estimator APIView
     """
 
-    def post(self, request):
+    def _save_log(self, log):
+        serializer = LogSerializer(data=log)
+        if serializer.is_valid():
+            serializer.save()
+
+    def post(self, request, res_fmt='json'):
         """
-        Return data as well as impact and severe impact estimations.
+        Return a list of all users.
         """
+        start = datetime.datetime.now()
+        log = {
+            'endpoint': self.request.META['PATH_INFO'],
+            'method': self.request.META['REQUEST_METHOD'],
+        }
         data = request.data
         res = estimator(data)
+        if res_fmt == 'xml':
+            end = datetime.datetime.now()
+            log['status'] = '200'
+            log['response_time'] = f"{str(get_time_diff(start, end))} ms"
+            self._save_log(log)
+            return Response(dicttoxml(res), status=status.HTTP_200_OK, content_type='application/xml')
+        else:
+            end = datetime.datetime.now()
+            log['status'] = '200'
+            log['response_time'] = f"{str(get_time_diff(start, end))} ms"
+            self._save_log(log)
+            return Response(res, status=status.HTTP_200_OK)
+
+
+class Log(APIView):
+    """ Logs Viewset"""
+
+    def _save_log(self, log):
+        serializer = LogSerializer(data=log)
+        if serializer.is_valid():
+            serializer.save()
+
+    def get(self, request):
+        start = datetime.datetime.now()
+        log = {
+            'endpoint': self.request.META['PATH_INFO'],
+            'method': self.request.META['REQUEST_METHOD'],
+        }
+        logs = Log.objects.all()
+        serializer = LogSerializer(logs, many=True)
+        end = datetime.datetime.now()
+        log['status'] = '200'
+        log['response_time'] = f"{str(get_time_diff(start, end))} ms"
+        self._save_log(log)
+        res = [f"{res['method']}    {res['endpoint']}   {res['status']}     {res['response_time']}"
+               for res in serializer.data]
         return Response(res, status=status.HTTP_200_OK)
-
-
-class LogViewSet(generics.ListCreateAPIView):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = Log.objects.all()
-    serializer_class = LogSerializer
